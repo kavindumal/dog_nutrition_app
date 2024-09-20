b92be8522f85a0fc45155e73ae81e07b4ff7b825
kavindumal <kavindu11250403@gmail.com>
2024-09-20 07:27:26 +0530

    Signup page swipe animation and swipe pages updated

--- lib/pages/SignupPage.dart
+++ lib/pages/SignupPage.dart
@@ -12,84 +12,226 @@ class SignupPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 30), // Top padding
-            Padding(
-              padding: const EdgeInsets.only(left: 35.0), // Moves the text 100 pixels to the right
-              child: Text(
-                "Meet your",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 45, // Increased font size
-                ),
-                textAlign: TextAlign.left,
+            Expanded(
+              child: PageView(
+                controller: controller,
+                children: [
+                  // Page 1
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 30), // Top padding
+                      Padding(
+                        padding: const EdgeInsets.only(left: 35.0),
+                        child: Text(
+                          "Meet your",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 45,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 35.0),
+                        child: Text(
+                          "dogs needs",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 45,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 35.0),
+                        child: Text(
+                          "here",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 45,
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Image.asset(
+                          'assets/images_ui/signup_page_img.png',
+                          width: 250,
+                          height: 250,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Center(
+                        child: Text(
+                          "Get interesting promos here, register your",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF898989),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          "account immediately so you can meet your",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF898989),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          "animal needs.",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF898989),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  // Page 2
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 30),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 35.0),
+                        child: Text(
+                          "Track your",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 45,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 35.0),
+                        child: Text(
+                          "dog’s health",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 45,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 35.0),
+                        child: Text(
+                          "progress",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 45,
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Image.asset(
+                          'assets/images_ui/health_tracking_img.png',
+                          width: 250,
+                          height: 250,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Center(
+                        child: Text(
+                          "Monitor your pet’s health, activity, and",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF898989),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          "nutrition to ensure their well-being.",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF898989),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  // Page 3
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 30),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 35.0),
+                        child: Text(
+                          "Discover our",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 45,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 35.0),
+                        child: Text(
+                          "community",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 45,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 35.0),
+                        child: Text(
+                          "now",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 45,
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Image.asset(
+                          'assets/images_ui/community_img.png',
+                          width: 250,
+                          height: 250,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Center(
+                        child: Text(
+                          "Discover new education futures for the",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF898989),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          "more knowledgement about your dog",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xFF898989),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 35.0), // Moves the text 100 pixels to the right
-              child: Text(
-                "animal needs",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 45, // Increased font size
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 35.0), // Moves the text 100 pixels to the right
-              child: Text(
-                "here",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 45, // Increased font size
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-            Center(
-              child: Image.asset(
-                'assets/images_ui/signup_page_img.png',
-                width: 250, // Adjust width if needed
-                height: 250, // Adjust height if needed
-              ),
-            ),
-            SizedBox(height: 20), // Spacing between image and text
-            Center(
-              child: Text(
-                "Get interesting promos here, register your",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Color(0xFF898989),
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Center(
-              child: Text(
-                "account immediately so you can meet your",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Color(0xFF898989),
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Center(
-              child: Text(
-                "animal needs.",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Color(0xFF898989),
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            SizedBox(height: 40), // Spacing between text and indicator
+
+            // SmoothPageIndicator
             Center(
               child: SmoothPageIndicator(
                 controller: controller,
                 count: 3,
-                axisDirection: Axis.horizontal,
                 effect: WormEffect(
                   spacing: 8.0,
                   radius: 30.0,
@@ -98,30 +240,30 @@ class SignupPage extends StatelessWidget {
                   paintStyle: PaintingStyle.stroke,
                   strokeWidth: 0.5,
                   dotColor: Colors.grey,
-                  activeDotColor: Color(0xFF0077B6), // Updated active color to #0077B6
+                  activeDotColor: Color(0xFF0077B6),
                 ),
               ),
             ),
             SizedBox(height: 40), // Spacing between indicator and button
             Center(
               child: SizedBox(
-                width: MediaQuery.of(context).size.width - 80, // Button width adjustment
-                height: 60, // Explicitly set button height
+                width: MediaQuery.of(context).size.width - 80,
+                height: 60,
                 child: ElevatedButton(
                   onPressed: () {
-                    // Button action here
+                    // Handle Get Started action
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF0077B6), // Button background color
+                    backgroundColor: Color(0xFF0077B6),
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25.36), // Button border radius
+                      borderRadius: BorderRadius.circular(25.36),
                     ),
-                    padding: EdgeInsets.zero, // Remove default padding to use SizedBox height
+                    padding: EdgeInsets.zero,
                   ),
                   child: Text(
                     "Get Started",
                     style: TextStyle(
-                      color: Colors.white, // Button text color
+                      color: Colors.white,
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
                     ),
